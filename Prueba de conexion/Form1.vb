3c42c7c80fc26220b74269821cdde7a65e893d9f
Emiliano Urbina <andurbina@gmail.com>
2021-07-14 12:49:48 -0600

    Version 1.0 de Prueba BD remota

---
+++ Prueba de conexion/Form1.vb
@@ -0,0 +1,17 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Dim conexion As New MySqlConnectionStringBuilder
+            conexion.Server = "us-cdbr-east-04.cleardb.com"
+            conexion.UserID = "bcd2e45ae5722c"
+            conexion.Password = "c9507fa2"
+            conexion.Database = "heroku_96a597346e8eaba"
+
+            Dim con As New MySqlConnection(conexion.ToString())
+            con.Open()
+        Catch ex As Exception
+            MsgBox("No se pudo conectar: " + ex.Message + "")
+        End Try
+    End Sub
+End Class
